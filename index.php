db97932e356d2ad2a9f146a5c82f3d3800621a53
Enrique Gonzalez <iquique.gm@gmail.com>
2023-11-10 12:51:33 -0600

    Ajustes Seleccion
    Ajustes a la rutina de seleccion de color.

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <title>Tienda</title>
 </head>
 <body>
+
+
     <?php
         include "conector.php";
         // Select data from table
@@ -22,7 +24,7 @@
         }
         }
         // Print array
-        print_r($productos);
+        // print_r($productos);
 
         
         // Select data from table
@@ -38,7 +40,7 @@
         }
         }
         // Print array
-        print_r($existencias);
+        // print_r($existencias);
 
         
         // Select data from table
@@ -54,7 +56,7 @@
         }
         }
         // Print array
-        print_r($colores);
+        // print_r($colores);
 
     ?>
     <form action="$" method="post" id="seleccion">
@@ -64,13 +66,13 @@
                 <option class="campotexto" nombre="<?php echo $producto['nombre'];?>"value="<?php echo $producto['id'];?>"><?php echo $producto['descripcion'];?></option>
             <?php }?>
         </select>
-        <img src="imagenes/LogoVortice.png" height="200px" id="imagenProducto" onchange="actualizaColores()">
+        <img src="imagenes/LogoVortice.png" height="200px" id="imagenProducto">
 
         <?php
 
             foreach ($colores as $valorColor) {
              
-                echo '<div class="color" id="color' . $valorColor['id'] . '"style="width:50px; height:50px;background:#'. $valorColor['valor'] . ';border-radius:50%;display:block;">0</div>';
+                echo '<div class="color" id="' . $valorColor['id'] . '"style="width:50px; height:50px;background:#'. $valorColor['valor'] . ';border-radius:50%;display:none;" onclick="seleccionaProducto(' . $valorColor['id'] . ')">0</div>';
        
             }
 
@@ -83,36 +85,47 @@
     </form>
 
     <script>
-            function cambiaImagen(){
 
-                //Cambio de Imagen
-                console.log("Imagen");
-                var nuevaImagen = document.getElementById("selector");
-                var productoSeleccionado = nuevaImagen.selectedOptions[0].value;
-                var imagen = document.getElementById("imagenProducto");
-                var nombre = <?php echo json_encode($productos); ?>;
-                console.log(nombre[(nuevaImagen.selectedOptions[0].value) - 1].nombre);
-                imagen.src = "imagenes/" + nombre[productoSeleccionado - 1].nombre + ".png";
+nombre = <?php echo json_encode($productos); ?>;
+modelo = <?php echo json_encode($existencias); ?>;
+color = <?php echo json_encode($colores); ?>;
 
-                //Cambio de Colores
-                console.log("Colores");
-                var modelo = <?php echo json_encode($existencias); ?>;
-                var color = <?php echo json_encode($colores); ?>;
-                document.querySelectorAll('.color').forEach(function(el) {
-                el.style.display = 'none';
-                });
-                modelo.forEach(function(modeloActual) {
-                    console.log(modeloActual.id_producto);
-                    console.log(productoSeleccionado);
-                    if (modeloActual.id_producto == productoSeleccionado) {
-                        var identificador = "color" + modeloActual.id_color;
-                        console.log(identificador);
-                        document.getElementById(identificador).style.display='block';
-                        document.getElementById(identificador).innerHTML = modeloActual.cantidad;
-                    }
-                });
-            }
 
-    </script>
+   function cambiaImagen(){
+ 
+    //Cambio de Imagen
+    console.log(nombre);
+    var nuevaImagen = document.getElementById("selector");
+    var productoSeleccionado = nuevaImagen.selectedOptions[0].value;
+    var imagen = document.getElementById("imagenProducto");
+    console.log(nombre[(nuevaImagen.selectedOptions[0].value) - 1].nombre);
+    imagen.src = "imagenes/" + nombre[productoSeleccionado - 1].nombre + ".png";
+
+    //Cambio de Colores
+    console.log("Colores");
+    document.querySelectorAll('.color').forEach(function(el) {
+    el.style.display = 'none';
+    });
+    modelo.forEach(function(modeloActual) {
+        console.log(modeloActual.id_producto);
+        console.log(productoSeleccionado);
+        if (modeloActual.id_producto == productoSeleccionado) {
+            var identificador = modeloActual.id_color;
+            console.log(identificador);
+            document.getElementById(identificador).style.display='block';
+            document.getElementById(identificador).innerHTML = modeloActual.cantidad;
+        }
+    });
+}
+
+function seleccionaProducto(id) {
+    var nuevaImagen = document.getElementById("selector");
+    var productoSeleccionado = nuevaImagen.selectedOptions[0].value;
+    console.log(nombre[productoSeleccionado - 1].descripcion);
+    var colorSeleccionado = color[id].nombre;
+    console.log(colorSeleccionado + id);
+}
+
+</script> 
 </body>
 </html>
